c2ea39b59352be53aa74686e97fe6e0c2e1598e6
WillyPuri <guglielmopuri@gmail.com>
2026-01-15 11:37:01 +0100

    Update symple_induction_head_dataset.py

--- symple_induction_head_dataset.py
+++ symple_induction_head_dataset.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import random
 from transformers import AutoTokenizer
+from tokenizers import Tokenizer, models, trainers, pre_tokenizers
 
 class TokenInductionHeadDataset(Dataset):
     def __init__(self, tokenizer, seq_len=30, dataset_size=1000,vocab_size=100, special_token=None):                 # Volendo si può usare un token (formato stringa) presente nel dizionario di tokenizer
@@ -44,3 +45,42 @@ class TokenInductionHeadDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.data[idx], self.targets[idx]
+
+
+def Make_Tokenizer():
+  vocab = [
+      "0","1","2","3","4","5","6","7","8","9",
+      "A","B","C","D","E","F","G","H","I","J","K","L","M","+"
+  ]
+
+  tokenizer = Tokenizer(models.WordLevel(unk_token="[UNK]"))                       # Creo il tokenizer più semplice possibile che converte una parola in un token
+  tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()                            # Gli sto dicendo di riconoscere gli spazi così posso dirgli: "A B C D" = ["A","B","C","D"]
+                                                                                   # invece "ABCD" viene inteso come "[UNK]"
+  trainer = trainers.WordLevelTrainer(                                             # Questo definisce il vocabolario aggiungendoci il token "[UNK]"
+      vocab_size=len(vocab) + 1,                                                   # +1 per [UNK]
+      special_tokens=["[UNK]"]
+  )
+  tokenizer.train_from_iterator(vocab, trainer)                                    # crea il tokenizer vero e proprio
+
+  return tokenizer
+
+def From_Seq_To_Numb(tokenizer,dataset):                                           # Questa funzione cambia il dataset da stringhe ad interi tramite il tokenizer
+  vocab = tokenizer.get_vocab()
+
+  data = []
+  targets = []
+
+  for seq, target in dataset:
+      seq_ids = [vocab[token] for token in seq]
+      target_id = vocab[target]
+
+      data.append(seq_ids)
+      targets.append([target_id])
+
+  data = np.array(data)
+  targets = np.array(targets)
+
+  data = torch.Tensor(data).long()
+  targets = torch.Tensor(targets).long()
+
+  return data,targets
