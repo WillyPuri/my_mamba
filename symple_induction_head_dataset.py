540eeaed0242ff0de0cc8d1e63655433e8ae8b35
WillyPuri <guglielmopuri@gmail.com>
2026-01-19 00:53:22 +0100

    Update symple_induction_head_dataset.py

--- symple_induction_head_dataset.py
+++ symple_induction_head_dataset.py
@@ -4,38 +4,39 @@ from torch.utils.data import Dataset
 import random
 from transformers import AutoTokenizer
 from tokenizers import Tokenizer, models, trainers, pre_tokenizers
+import string
 
 class TokenInductionHeadDataset(Dataset):
-    def __init__(self, tokenizer, seq_len=30, dataset_size=1000,vocab_size=100, special_token=None):                 # Volendo si può usare un token (formato stringa) presente nel dizionario di tokenizer
+    def __init__(self, tokenizer, seq_len=30, dataset_size=1000,vocab_size=100, special_token="A", num_special_tokens = 1):                 # Volendo si può usare un token (formato stringa) presente nel dizionario di tokenizer
         self.tokenizer = tokenizer
         self.seq_len = seq_len
         self.dataset_size = dataset_size
         self.vocab_size = vocab_size
-
-        if special_token is None:
-            special_token = '+'
-
         self.special_token = special_token
-
+        self.num_special_tokens = num_special_tokens
         self.data = []
         self.targets = []
-
         self._generate_data()
 
     def _generate_data(self):
       all_tokens = list(self.tokenizer.get_vocab().keys())[:self.vocab_size]
+      print("Set of special tokens: ",all_tokens[:self.num_special_tokens])
       for _ in range(self.dataset_size):
         seq = []
+        if self.special_token == "random":
+          special_tok = random.choice(all_tokens[:self.num_special_tokens])  # Non tutto l'alfabeto è utilizzabile come special tokens
+        else:
+          special_tok = self.special_token
 
-        for i in range(self.seq_len):
-          tok = random.choice(all_tokens)
-          while tok == self.special_token:
+        for i in range(self.seq_len-1):                                   # seq_len-1 perchè l'ultimo token sarà quello speciale
+          tok = random.choice(all_tokens)                         
+          while tok == special_tok:                                # Evito che lo special token sia ripetuto più volte
             tok = random.choice(all_tokens)
           seq.append(tok)
-        seq.append(self.special_token)                                      #l'ultimo token è quello speciale
+        seq.append(special_tok)                                    # l'ultimo token è quello speciale
 
-        pos = random.randint(0, self.seq_len - 1)
-        seq[pos] = self.special_token
+        pos = random.randint(0, self.seq_len - 2)
+        seq[pos] = special_tok
         target = seq[pos + 1]
 
         self.data.append(seq.copy())
@@ -46,20 +47,26 @@ class TokenInductionHeadDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.data[idx], self.targets[idx]
-        
-def Make_Tokenizer():
-  vocab = [
-      "0","1","2","3","4","5","6","7","8","9",
-      "A","B","C","D","E","F","G","H","I","J","K","L","M","+"
-  ]
 
-  tokenizer = Tokenizer(models.WordLevel(unk_token="[UNK]"))                       # Creo il tokenizer più semplice possibile che converte una parola in un token
+def Make_Tokenizer(vocab_size):
+  caratteri = (
+        string.ascii_uppercase +
+        string.ascii_lowercase +
+        string.digits +
+        string.punctuation
+    )
+  vocab = list(caratteri)
+
+  if vocab_size > len(vocab):
+    extra = vocab_size - len(vocab)
+    vocab.extend(str(i+10) for i in range(extra))                                  # Nel caso vocab_size sia troppo grande => aggiungo numeri al vocabolario
+ 
+  vocab = vocab[:vocab_size]
+
+  tokenizer = Tokenizer(models.WordLevel())                                        # Creo il tokenizer più semplice possibile che converte una parola in un token
   tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()                            # Gli sto dicendo di riconoscere gli spazi così posso dirgli: "A B C D" = ["A","B","C","D"]
-                                                                                   # invece "ABCD" viene inteso come "[UNK]"
-  trainer = trainers.WordLevelTrainer(                                             # Questo definisce il vocabolario aggiungendoci il token "[UNK]"
-      vocab_size=len(vocab) + 1,                                                   # +1 per [UNK]
-      special_tokens=["[UNK]"]
-  )
+
+  trainer = trainers.WordLevelTrainer(vocab_size=len(vocab))
   tokenizer.train_from_iterator(vocab, trainer)                                    # crea il tokenizer vero e proprio
 
   return tokenizer
@@ -84,3 +91,12 @@ def From_Seq_To_Numb(tokenizer,dataset):                                        
   targets = torch.Tensor(targets).long()
 
   return data,targets
+
+def print_sequence(seq, special_token):
+    colored_seq = []
+    for tok in seq:
+        if tok == special_token:
+            colored_seq.append(f"{RED}{tok}{RESET}")
+        else:
+            colored_seq.append(tok)
+    print(" ".join(colored_seq))
