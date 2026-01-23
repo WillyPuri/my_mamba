4dbd87019c947f28ae32d09aa23c441318e5565f
WillyPuri <guglielmopuri@gmail.com>
2026-01-23 12:12:31 +0100

    Update symple_induction_head_dataset.py

--- symple_induction_head_dataset.py
+++ symple_induction_head_dataset.py
@@ -87,8 +87,8 @@ def From_Seq_To_Numb(tokenizer,dataset):                                        
   data = np.array(data)
   targets = np.array(targets)
 
-  data = torch.Tensor(data).long()
-  targets = torch.Tensor(targets).long()
+  data = torch.Tensor(data).int()
+  targets = torch.Tensor(targets).int()
 
   return data,targets
 
